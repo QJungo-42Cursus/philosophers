a977a43fac05b5de87fe4915f899e9b3112374f7
Quentin Jungo <qjungo@student.42lausanne.ch>
2023-03-13 14:40:34 +0100

    fix print died second time

--- philo/ft_log.c
+++ philo/ft_log.c
@@ -6,15 +6,36 @@
 /*   By: qjungo <qjungo@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 10:04:06 by qjungo            #+#    #+#             */
-/*   Updated: 2023/03/13 10:04:21 by qjungo           ###   ########.fr       */
+/*   Updated: 2023/03/13 14:38:48 by qjungo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 #include <stdio.h>
 
+static int	ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (((unsigned char)str1[i] || (unsigned char)str2[i]) && i < n)
+	{
+		if ((unsigned char)str1[i] > (unsigned char)str2[i])
+			return (1);
+		if ((unsigned char)str1[i] < (unsigned char)str2[i])
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
 void	ft_log(t_philosopher *philosopher, char *msg)
 {
+	static t_bool	one_died = FALSE;
+
+	if (one_died)
+		return ;
+	one_died = (ft_strncmp(msg, "died", 5) == 0);
 	pthread_mutex_lock(&(philosopher->program->print_mutex));
 	printf("%ld %d %s\n",
 		get_timestamp_from_start(philosopher->program->start_timestamp),
